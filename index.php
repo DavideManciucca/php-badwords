03d7abf7c62167cf5d9140a2fbf7f81808c7c07a
DavideManciucca <davidemanciucca@gmail.com>
2022-06-06 16:21:57 +0200

    esercizio terminato

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+$text=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, aperiam nihil dignissimos eos maxime prova quis iusto itaque magnam sapiente repellat! prova Eligendi, prova reiciendis consequatur? Nobis, error consequuntur. Quibusdam perspiciatis eligendi ab?"
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primo php</title>
+</head>
+<body>
+  <p>
+    il testo è : <?php echo $text ?>
+</p>
+<p>Questo paragrafo è lungo <?php echo strlen($text) ?> caratteri </p>
+<p>Adesso censuriamo la parola "prova" <br> <?php echo str_replace("prova", "***", $text) ?> </p>
+ 
+</body>
+</html>
